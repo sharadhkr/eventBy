62d187cb150928ec59bf99055d1aca4344e63b8f
Sharad <www.sharadrathoreji@gmail.com>
2026-01-18 10:03:08 +0530

    c

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5174", "http://localhost:5173"],
+    origin: ["http://localhost:5174", "http://localhost:5173","https://eventby-1.onrender.com"],
     credentials: true,
      allowedHeaders: ['Content-Type', 'Authorization']
   })
